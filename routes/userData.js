6896aa7afff428cd59ad0bce4d118299a4d64a0b
iamkevinjames <75653990+iamkevinjames@users.noreply.github.com>
2022-09-25 00:28:49 +0530

    Update userData.js

--- routes/userData.js
+++ routes/userData.js
@@ -1,48 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const userDataModel = require("../models/userDataModel");
 
-router.get("/", async (req, res) => {
-  try {
-    const userData = await userDataModel.find();
-    res.json(userData);
-  } catch (err) {
+const data = {
+  firstName: "Vladimir",
+  lastName: "Putin",
+  emailAddress: "vladmirputin@gmail.com",
+};
+
+router.get("/getData", (req, res) => {
+  try{
+  res.send(data);
+  }catch (err) {
     res.send("Error: " + err.message);
   }
 });
 
-router.get("/:id", async (req, res) => {
-  try {
-    const userData = await userDataModel.findById(req.params.id);
-    res.json(userData);
-  } catch (err) {
+router.post("/postData", (req, res) => {
+  try{
+  data.firstName = req.body.firstName;
+  data.lastName = req.body.lastName;
+  data.emailAddress = req.body.emailAddress;
+  console.log(req.body);
+  res.send("User Data updated successfully!");
+  }catch (err) {
     res.send("Error: " + err.message);
-  }
-});
-
-router.post("/", async (req, res) => {
-  const userData = new userDataModel({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    emailAddress: req.body.emailAddress,
-  });
-
-  try {
-    const userData1 = await userData.save();
-    res.json(userData1);
-  } catch (err) {
-    res.send(err);
-  }
-});
-
-router.patch("/:id", async (req, res) => {
-  try {
-    const userData = await userDataModel.findById(req.params.id);
-    userData.emailAddress = req.body.emailAddress;
-    const userData1 = await userData.save();
-    res.json(userData1);
-  } catch (err) {
-    res.send(err);
   }
 });
 
