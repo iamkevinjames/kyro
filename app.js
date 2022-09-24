c6b25b14d38a777ac7a1618280315b767bf94730
iamkevinjames <75653990+iamkevinjames@users.noreply.github.com>
2022-09-25 00:26:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,17 +13,9 @@ const data = {
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/getData", (req, res) => {
-  res.send(data);
-});
 
-app.post("/postData", (req, res) => {
-  data.firstName = req.body.firstName;
-  data.lastName = req.body.lastName;
-  data.emailAddress = req.body.emailAddress;
-  console.log(req.body);
-  res.send("User Data updated successfully!");
-});
+
+app.use(`/.netlify/functions/api`, userRouter);
 
 app.listen(8000, () => {
   console.log("API Server is running...");
